5f1c8e3836f468d3deb2d97a7f408e51e8fd7203
Anubhav Sharma <shrmaanubhav94@gmail.com>
2026-01-24 07:33:46 +0000

    minor fixes

--- client/cyber_lens/src/App.tsx
+++ client/cyber_lens/src/App.tsx
@@ -1,9 +1,4 @@
-import {
-  BrowserRouter as Router,
-  Routes,
-  Route,
-  Outlet,
-} from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route, Outlet } from "react-router-dom";
 
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
@@ -11,16 +6,14 @@ import Footer from "./components/Footer";
 import Home from "./pages/Home";
 import History from "./pages/History";
 import News from "./pages/News";
+import NewsDetail from "./pages/NewsDetail";
+import Analytics from "./pages/Analytics";
+
 import Login from "../../../contributors/MK-codes365/pages/Login";
 import Signup from "../../../contributors/MK-codes365/pages/Signup";
 import VerifyEmail from "../../../contributors/MK-codes365/pages/VerifyEmail";
-import Analytics from "./pages/Analytics";
-import NewsDetail from "./pages/NewsDetail";
 
-// import Login from "./pages/Login";
-// import Signup from "./pages/Signup";
-// import VerifyEmail from "./pages/VerifyEmail";
-
+/* ---------- Shared Layout (Navbar + Footer) ---------- */
 const Layout = () => {
   return (
     <div className="min-h-screen flex flex-col">
@@ -37,23 +30,19 @@ function App() {
   return (
     <Router>
       <Routes>
+        {/* -------- App Pages (with Navbar & Footer) -------- */}
         <Route element={<Layout />}>
           <Route path="/" element={<Home />} />
           <Route path="/history" element={<History />} />
           <Route path="/news" element={<News />} />
           <Route path="/news/:id" element={<NewsDetail />} />
           <Route path="/analytics" element={<Analytics />} />
-          <Route path="/login" element={<Login />} />
-          <Route path="/signup" element={<Signup />} />
-          <Route path="/verify-email" element={<VerifyEmail />} />
-          <Route path="/analytics" element={<Analytics />} />
         </Route>
 
-        {/*
+        {/* -------- Auth Pages (NO Navbar / Footer) -------- */}
         <Route path="/login" element={<Login />} />
         <Route path="/signup" element={<Signup />} />
         <Route path="/verify-email" element={<VerifyEmail />} />
-        */}
       </Routes>
     </Router>
   );
